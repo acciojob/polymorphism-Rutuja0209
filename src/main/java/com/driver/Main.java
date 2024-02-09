ec272f18291da714f6381d2de0306b63cce6a29e
spd1512 <desaishami99@gmail.com>
2024-02-10 00:02:50 +0530

    Adding some changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,10 +4,29 @@ public class Main {
     //1. create class Product inside main class
     public static void main(String[] args) {
         //2. create object of product in main function called p
-        Product p = new Product();
-    }
-    static class Product{
+        Main.Product p = new Main.Product();
+        //Product p = new Product();
+        int ans = p.product(10, 20);
+
+        int ans1 = p.product(1, 2, 3);
+
+        double ans2 = p.product(1.0, 2.0);
 
     }
+    
+    //1.
+    public static class Product{
+        public int product(int x, int y){
+            return x*y;
+
+        }
+
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+
+        public double product(double x, double y){
+        return x*y;
+        }
 
 }
