4a2204de3fc3600dcd5a7b343716f604ba89cc80
spd1512 <desaishami99@gmail.com>
2024-02-09 23:45:19 +0530

    Adding some changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    //1. create class Product inside main class
+    public static void main(String[] args) {
+        //2. create object of product in main function called p
+        Product p = new Product();
+    }
+    static class Product{
+
+    }
 
 }
